0e92d96ef609b19d20e803c3a393235454fd03c4
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-13 09:54:57 -0300

    feat: adiciona controllers e rotas pegaUmaMatricula

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -27,8 +27,8 @@ class PessoaController {
       return res.status(200).json(novaPessoaCriada);
     } catch(error) {
       return res.status(500).json(error.message);
-    }
-  }
+    };
+  };
 
   static async atualizaPessoa(req, res) {
     const novasInfos = req.body;
@@ -39,8 +39,8 @@ class PessoaController {
       return res.status(200).json(pessoaAtualizada);
     } catch(error) {
       return res.status(500).json(error.message);
-    }
-  }
+    };
+  };
 
   static async apagaPessoa(req, res) {
     const { id } = req.params;
@@ -49,8 +49,25 @@ class PessoaController {
       return res.status(200).json({mensagem: `ìd ${id} deletado`});
     } catch(error) {
       return res.status(500).json(error.message);
-    }
-  }
+    };
+  };
+
+  static async pegaUmaMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      const umaMatricula = await database.Matriculas.findOne( 
+        { 
+          where: 
+          { 
+            id: Number(matriculaId),
+            estudante_id: Number(estudanteId)
+          }
+        });
+      return res.status(200).json(umaMatricula);
+    } catch(error){
+      return res.status(500).json(error.message);
+    };
+  };
 }
 
 module.exports = PessoaController;
