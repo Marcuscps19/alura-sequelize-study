d589acd688c992d113965506d63e88216451e48e
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-13 00:17:07 -0300

    feat: finaliza rota de criar nova pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -7,15 +7,24 @@ class PessoaController {
       return res.status(200).json(todasAsPessoas);
     } catch (error) {
       return res.status(500).json(error.message);
-    }
-  }
+    };
+  };
 
-  static async pegaUmaPessoa(req, res){
+  static async pegaUmaPessoa(req, res) {
     const { id } = req.params;
     try {
       const umaPessoa = await database.Pessoas.findOne( { where: { id: Number(id) }});
       return res.status(200).json(umaPessoa);
     } catch(error){
+      return res.status(500).json(error.message);
+    };
+  };
+  static async criaPessoa(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const nvoaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(nvoaPessoaCriada);
+    } catch(error) {
       return res.status(500).json(error.message);
     }
   }
