140511abb22f3e9b99ca7d4db2e350a6b519a5c5
Marcus Hüsemann <mchusemann@gmail.com>
2021-10-12 23:20:09 -0300

    feat: organiza as rotas

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
 
-app.use(bodyParser.json());
-
 const PORT = 3000;
 
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ message: 'Welcome' }));
+routes(app);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
 
